dbdf57390973b1506b0811751c189fd895a81574
DylanSwift56 <dylanswift56@gmail.com>
2024-04-05 12:12:17 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import render_template
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
